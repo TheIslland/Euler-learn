274f9c79259e9a03cf01b6a57f48b355854cfdce
TheIsland <41140112+TheIslland@users.noreply.github.com>
2018-07-22 10:14:32 +0800

    Add files via upload

---
+++ oula5.c
@@ -0,0 +1,12 @@
+mZ
+	> File Name: oula5.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年07月22日 星期日 09时39分21秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    
+    return 0;
+}
